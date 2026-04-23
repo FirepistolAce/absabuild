8cb992c3ee4744f8449b33eca93c973033e6faf5
Theo <162183748+FirepistolAce@users.noreply.github.com>
2026-04-23 20:28:30 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StrategyTrack from './Pages/StrategyTrack'
 import SimulationLab from './Pages/SimulationLab'
 import './App.css'
 
-function App() {
+function App() { 
   return (
     <UserProvider>
       <div className="app">
